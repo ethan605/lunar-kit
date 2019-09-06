2d0eac10707b38898ca5322eee9948c61ab77dfa
Ethan Nguyen <thanhnx.605@gmail.com>
2019-09-06 22:37:35 +0700

    Refine

--- packages/converters/__tests__/LunarDate.spec.ts
+++ packages/converters/__tests__/LunarDate.spec.ts
@@ -1,11 +1,11 @@
 import LunarDate from '../src/LunarDate';
 
 // Fixtures
-import conversionFixtures from './fixtures/conversions.json';
+import fixtures from './fixtures/conversions.json';
 
 describe('LunarDate', () => {
   it('should convert to solar dates correctly', () => {
-    conversionFixtures.forEach(({ solar, timeZone, lunar, leapMonth }) => {
+    fixtures.forEach(({ solar, timeZone, lunar, leapMonth }) => {
       const [day, month, year] = lunar;
       const lunarDate = new LunarDate({ day, month, year, isLeapMonth: leapMonth });
       const solarDate = lunarDate.toSolarDate(timeZone);
